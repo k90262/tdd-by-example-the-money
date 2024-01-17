e79a124436ea20ddc6bbab31a035e191e40035c8
Bill Ho <ycho.bill.lab@gmail.com>
2024-01-18 00:55:26 +0800

    [ch1] added task list after finish ch1 multi-currency practice

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -2,6 +2,14 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/*
+* Task List:
+* 1. When exchange rate is 2:1 exchange rate between CHF and USD, 5 USD + 10 CHF = 10 USD.
+* 2. ~~5 USD * 2 = 10 USD~~ (DONE)
+* 3. Declare 'amount' to private.
+* 4. Any side effect on the Dollar class?
+* 5. Should 'amount' be an integer?
+* */
 public class MoneyTest {
     @Test
     public void testMultiplication() {
