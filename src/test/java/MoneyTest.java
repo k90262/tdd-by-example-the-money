1c758c0a038a54123a4ae51e598ffc57fffc2687
Bill Ho <ycho.bill.lab@gmail.com>
2024-01-19 00:51:59 +0800

    [ch2] object degeneracy

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -7,14 +7,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 * 1. When exchange rate is 2:1 exchange rate between CHF and USD, 5 USD + 10 CHF = 10 USD.
 * 2. ~~5 USD * 2 = 10 USD~~ (DONE)
 * 3. Declare 'amount' to private.
-* 4. Any side effect on the Dollar class?
+* 4. ~~Any side effect on the Dollar class?~~ (DONE)
 * 5. Should 'amount' be an integer?
 * */
 public class MoneyTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
     }
 }
