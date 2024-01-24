a89346c8b387d509bbf4d8fc69df817280c357fb
Bill Ho <ycho.bill.lab@gmail.com>
2024-01-25 00:46:48 +0800

    [ch5] CHF : Hi, I am here

---
+++ src/main/java/Franc.java
@@ -0,0 +1,16 @@
+public class Franc {
+    public int amount;
+
+    public Franc(int amount) {
+        this.amount = amount;
+    }
+
+    public Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+
+    public boolean equals(Object object) {
+        Franc dollar = (Franc) object;
+        return amount == dollar.amount;
+    }
+}
--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -5,14 +5,18 @@ import static org.junit.jupiter.api.Assertions.*;
 /*
 * Task List:
 * 1. When exchange rate is 2:1 exchange rate between CHF and USD, 5 USD + 10 CHF = 10 USD.
-* 2. ~~5 USD * 2 = 10 USD~~
-* 3. ~~Declare 'amount' to private.~~
-* 4. ~~Any side effect on the Dollar class?~~
+* 2. ~~ 5 USD * 2 = 10 USD ~~
+* 3. ~~ Declare 'amount' to private. ~~
+* 4. ~~ Any side effect on the Dollar class? ~~
 * 5. Should 'amount' be an integer?
-* 6. ~~equals()*~~
+* 6. ~~ equals()* ~~
 * 7. hashCode()*
 * 8. check null via equals()*
 * 9. check any type via equals()*
+* 10. ~~ 5 CHF * 2 = 10 CHF ~~
+* 11. Redundant Design betwen USD Dollar and CHF Franc
+* 12. ** Shared equal() method **
+* 13. Shared times() method
 *
 * NOTE:
 * a. ~~ DONE_ITEM ~~
@@ -31,5 +35,12 @@ public class MoneyTest {
     public void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
+    }
+
+    @Test
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
     }
 }
